4edef800476fab0c6da6f26136cea6f223803161
Sahil <sahilofficial0709@gmail.com>
2025-03-20 23:32:17 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
-int main(){
+
+void bubblesort(int arr[], int n);
+void printarray(int arr[], int size);
+
+int main() {
     int n;
     scanf("%d", &n);
     int arr[n];
-    for(int i=0; i<n; i++){
+    
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    bubbleshort(arr, n);
+
+    bubblesort(arr, n);
     printarray(arr, n);
+
+    return 0;
 }
 
-void bubbleshort(int arr[], int n){
+
+void bubblesort(int arr[], int n) {
     int i, j, temp;
     int swapped;
 
-    for (i=0; i<n-i; i++){
+    for (i = 0; i < n - 1; i++) { 
         swapped = 0;
-        for(j = 0; j<n-i-1; j++){
-            if (arr[j] > arr[j+1]){
+
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-
                 swapped = 1;
             }
         }
-        if (swapped == 0);
+
+        if (swapped == 0) 
             break;
     }
 }
 
-void printarray(int arr[], int size){
-    for(int i=0; i<size; i++){
-        printf("%d", arr[i]);
-    printf("\n");
+void printarray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]); 
     }
+    printf("\n"); 
 }
