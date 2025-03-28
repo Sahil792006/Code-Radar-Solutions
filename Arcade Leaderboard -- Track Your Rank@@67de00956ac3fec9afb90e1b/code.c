ce4b8d502f1b8d3469121778703735f62468b56a
Sahil <sahilofficial0709@gmail.com>
2025-03-28 19:46:43 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
-void trackPlayerRanks(int ranked[], int player[], int m, int result[]){
-    int uniqueranked[200000], uniqueSize = 0; 
-    uniqueranked[0]=ranked[0];
-    uniqueSize = 1;
+void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]){
+    // Remove duplicates from ranked array
+    int uniqueranked[200000], uniqueSize = 0;
+    
+    if (n > 0) { // Ensure n is greater than 0 before accessing elements
+        uniqueranked[0] = ranked[0];
+        uniqueSize = 1;
+    }
+
     for(int i = 1; i < n; i++){
-        if(ranked[i] != ranked[i-1]){
+        if(ranked[i] != ranked[i - 1]){
             uniqueranked[uniqueSize++] = ranked[i];
         }
     }
 
+    // Initialize rank and index
     int rank = uniqueSize + 1;
     int index = uniqueSize - 1;
 
-    for(int i=0; i<m; i++){
+    for(int i = 0; i < m; i++){
         while (index >= 0 && player[i] >= uniqueranked[index]){
             index--;
             rank--;
@@ -22,25 +28,28 @@ void trackPlayerRanks(int ranked[], int player[], int m, int result[]){
     }
 }
 
-
 int main(){
     int n, m;
     scanf("%d", &n);
     int ranked[200000];
-    for(int i = 0; i<n; i++){
+
+    for(int i = 0; i < n; i++){
         scanf("%d", &ranked[i]);
     }
 
     scanf("%d", &m);
     int player[200000];
-    for (int i=0; i<m; i++){
+
+    for(int i = 0; i < m; i++){
         scanf("%d", &player[i]);
     }
 
     int result[200000];
     trackPlayerRanks(ranked, n, player, m, result);
-    for(int i=0; i<m; i++){
-        printf("%d\n", result[i]);
-    } 
-}
 
+    for(int i = 0; i < m; i++){
+        printf("%d\n", result[i]);
+    }
+
+    return 0;
+}
